e2513c639220a23b1b7a95d057b9c972a11248b4
Mortpo <anquetin.r@gmail.com>
2021-01-09 19:40:36 +0100

    le mal de tête

--- testerod.py
+++ testerod.py
@@ -23,15 +23,22 @@ for i in range(pictureX):
 
             for x in range(matrixX):
                 for y in range(matrixY):
-                    inequal = False
-                    if binaryPicture[0][0][0] != erodeMatrix[x][y] :
-                        result[i][j][0] = 0
-                        inequal = True
-            if inequal == False:
+                    valide = True
+                    if ((i-centerX) <= 0) or ((j-centerY)<=0) or ((i+centerX) >= pictureX) or ((j+centerY)>=pictureY): #verifie si on est dedans l'image
+                        valide = False
+                    print(i, j , i+x-centerX ,i+y-centerY)
+                    if valide == True and binaryPicture[i+x-centerX][i+y-centerY] != erodeMatrix[x][y] : #verifie si le filtre est vilidé
+                        valide = False
+            if valide == True:
+                result[i][j][0] = 0
+            else:
                 result[i][j][0] = 1
 
+
+plt.figure(0)
 plt.imshow(binaryPicture,cmap='binary')
-plt.show()
+plt.figure(1)
 plt.imshow(result,cmap='binary')
 plt.show()
+
                 
